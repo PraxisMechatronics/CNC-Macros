3152730e8813d2b2206555f015e386a0b4856d6b
PraxisMechatronics <140285658+PraxisMechatronics@users.noreply.github.com>
2024-01-10 19:48:52 -0500

    readme updated to .rs

--- README.rs
+++ README.rs
@@ -1,4 +1,9 @@
-# CNC-Macros
- 
+==========
+CNC-Macros
+==========
+
+Praxis Mechatronics NC Programs
+===============================
+
 This repo is for collecting all the macro-heavy multipurpose programs I have written 
 in the field.
